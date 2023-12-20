ae83541b4edfa2420f8a5a4bfd941179ab445c44
Hariharan-s005 <h.shakthivelrajagopal@cdw.com>
2023-12-20 16:40:10 +0530

    reorder imports in app.js

--- sitback-furniture/src/App.js
+++ sitback-furniture/src/App.js
@@ -1,23 +1,19 @@
-import HomePage from "./pages/HomePage/HomePage.jsx";
-import  ShoppingPage  from './pages/ShoppingPage/ShoppingPage.jsx';
-import { Routes, Route } from "react-router-dom";
-import OrderPage from "./pages/OrderPage/OrderPage.jsx";
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import ShoppingPage from './pages/ShoppingPage/ShoppingPage';
+import OrderPage from './pages/OrderPage/OrderPage';
 
 function App() {
   return (
-
-      <Routes>
-        <Route path="/" element={<HomePage />}>
-          <Route path="home" element={<HomePage />}/>
-        </Route>
-
-        <Route path="/categories">
-          <Route path=":categories" element={<ShoppingPage />} />
-        </Route>
-        <Route path="/confirmOrder" element={<OrderPage />} />
-      </Routes>
-    
- 
+    <Routes>
+      <Route path="/" element={<HomePage />}>
+        <Route path="home" element={<HomePage />} />
+      </Route>
+      <Route path="/categories">
+        <Route path=":categories" element={<ShoppingPage />} />
+      </Route>
+      <Route path="/confirmOrder" element={<OrderPage />} />
+    </Routes>
   );
 }
 
